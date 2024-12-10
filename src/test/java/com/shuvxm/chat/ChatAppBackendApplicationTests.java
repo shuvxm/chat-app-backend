fe0eeeb8a4432002d0be00c9060ee4fdc38804bd
shuvxm <kumarshubham5559@gmail.com>
2024-12-10 14:10:10 +0530

    "up"

---
+++ src/test/java/com/shuvxm/chat/ChatAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuvxm.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
