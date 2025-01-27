7c7768ececf469a649075112cd8376e95b22b225
shuvxm <kumarshubham5559@gmail.com>
2025-01-27 09:56:32 +0530

    "up"

--- src/main/java/com/shuvxm/chat/controllers/RoomController.java
+++ src/main/java/com/shuvxm/chat/controllers/RoomController.java
@@ -2,29 +2,45 @@ package com.shuvxm.chat.controllers;
 
 import com.shuvxm.chat.service.RoomService;
 import com.shuvxm.chat.entity.Room;
+import com.shuvxm.chat.repository.RoomRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/rooms")
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:5173")
 public class RoomController {
 
 	@Autowired
 	private RoomService roomService;
+	
+	@Autowired
+	private RoomRepository roomRepository;
 
 	// create room
+//	@PostMapping
+//	public ResponseEntity<?> createRoom(@RequestBody String roomId) {
+//		return roomService.createRoom(roomId);
+//	}
 	@PostMapping
 	public ResponseEntity<?> createRoom(@RequestBody String roomId) {
-		return roomService.createRoom(roomId);
+	    roomId = roomId.replace("\"", "").trim(); // Remove extra quotes
+//	    System.out.println("Creating room with ID: " + roomId);
+	    return roomService.createRoom(roomId);
 	}
 
+
+	
 	// get room : join
+//	return roomService.findByRoomId(roomId);
+
 	@GetMapping("/{roomId}")
 	public ResponseEntity<?> joinRoom(@PathVariable String roomId) {
-		return roomService.findByRoomId(roomId);
+		return roomService.joinRoom(roomId);
 	}
+
 
 	// get messages of room
 	@GetMapping("/{roomId}/messages")
